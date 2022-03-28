9b1988fb9d34c127467ebc53de84eb1d5bc9634a
Shikhar Sahdev <shikharsahdev@Shikhars-MacBook-Pro.local>
2022-03-29 03:41:35 +0530

    fireflies

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ import Intro from './pages/intro';
 function App() {
   return (
     <div className="App">
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
+    <div class="firefly"></div>
       <Banner />
       <Experience />
       <Intro />
